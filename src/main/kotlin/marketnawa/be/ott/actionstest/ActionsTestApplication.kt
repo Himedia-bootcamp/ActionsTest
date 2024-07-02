b9d6293d95d88f691ca185f829aac665de23d27b
악덕 <rudejrqkr97@gmail.com>
2024-07-02 10:18:18 +0900

    :tada: init

---
+++ src/main/kotlin/marketnawa/be/ott/actionstest/ActionsTestApplication.kt
@@ -0,0 +1,11 @@
+package marketnawa.be.ott.actionstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionsTestApplication>(*args)
+}
---
+++ src/test/kotlin/marketnawa/be/ott/actionstest/ActionsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package marketnawa.be.ott.actionstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
